6d2f9ce1fd105f6569c73c864bd58f170078a15e
Shadab Mizan <shadabsmizan@gmail.com>
2024-07-14 19:10:57 -0700

    Tested new shared ptr implementation with triangulated vertices.

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,9 @@
 
 int main(int argc, char const *argv[])
 {
-    Cube block1{"Block_1"};
+    Camera camera{Vec3f(8.49, 15.41, 6.46), Vec3f(79, 0, 154.4)};
+    Cube block1{"Block_1", Colour::BLUE};
 
-    std::cout << block1 << std::endl;
-
-    Cube block2{"Block_2", Colour(255,0,0)};
-    block2.setColour(3, Colour(0, 255, 0));
-    std::cout << block2 << std::endl;
-    
-    Cube block3{"Block_3", Colour::BLUE};
-    std::cout << block3 << std::endl;
-
-    
+    std::cout << block1;
     return 0;
 }
