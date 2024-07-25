3ee199a81f827439beb73770c96bfa1fe7d3c258
Shadab Mizan <shadabsmizan@gmail.com>
2024-07-25 09:39:51 -0700

    Restructured Colour struct so that it inherits Vec3. Made some data members public.

--- include/Vertex.h
+++ include/Vertex.h
@@ -3,20 +3,20 @@
 #include "geometry.h"
 #include <iostream>
 
-struct Colour
+struct Colour : public Vec3<unsigned char>
 {
     Colour();
+    Colour(unsigned char c);
+    // Here, xyz values of the vector are used as rgb respectively.
     Colour(unsigned char r, unsigned char g, unsigned char b);
 
     static const Colour RED;
     static const Colour GREEN;
     static const Colour BLUE;
 
-    unsigned char R, G, B;
-
     friend std::ostream& operator<<(std::ostream& os, const Colour& colour)
     {
-        os << "(" << colour.R << ", " << colour.G << ", " << colour.B << ")";
+        os << "(" << colour.x << ", " << colour.y << ", " << colour.z << ")";
         return os;
     }
 };
@@ -28,12 +28,6 @@ public:
     Vertex(float x, float y, float z);
     Vertex(Vec3f point);
     Vertex(Vec3f point, Colour colour);
-
-    void setColour(Colour Colour);
-    void zInterpolate();
-    const Colour getColour() const;
     
-     
-private:
-    Colour _colour;
+    Colour colour;
 };
--- src/Vertex.cpp
+++ src/Vertex.cpp
@@ -1,26 +1,17 @@
 #include "Vertex.h"
 
 // Constructs a black colour
-Colour::Colour() : R{0}, G{0}, B{0} {}
+Colour::Colour() : Vec3<unsigned char>() {}
 
+Colour::Colour(unsigned char c) : Vec3<unsigned char>(c) {}
 // Constructor for a desired Colour
-Colour::Colour(unsigned char red, unsigned char green, unsigned char blue) : R{red}, G{green}, B{blue} {}
+Colour::Colour(unsigned char red, unsigned char green, unsigned char blue) : Vec3<unsigned char>(red, green, blue) {}
 
 const Colour Colour::RED = Colour(255,0,0);
 const Colour Colour::GREEN = Colour(0, 255, 0);
 const Colour Colour::BLUE = Colour(0, 0, 255);
 
 Vertex::Vertex() : Vec3f() {}
-Vertex::Vertex(float x, float y, float z) : Vec3f(x,y,z), _colour{Colour()} {}
-Vertex::Vertex(Vec3f point) : Vec3f(point), _colour{Colour()} {}
-Vertex::Vertex(Vec3f point, Colour colour) : Vec3f(point), _colour{colour} {}
-
-void Vertex::setColour(Colour colour) { _colour = colour; }
-const Colour Vertex::getColour() const { return _colour; }
-
-void Vertex::zInterpolate()
-{
-    _colour.R /= this->z;
-    _colour.G /= this->z;
-    _colour.B /= this->z;
-}
+Vertex::Vertex(float x, float y, float z) : Vec3f(x,y,z), colour{Colour()} {}
+Vertex::Vertex(Vec3f point) : Vec3f(point), colour{Colour()} {}
+Vertex::Vertex(Vec3f point, Colour colour) : Vec3f(point), colour{colour} {}
